24579612733a9b71a4525f0c948995a2163981a2
meowaura <118028413+meowaura@users.noreply.github.com>
2024-12-19 09:40:57 +0300

    Update task3.c

--- 3824Б1ПР6/16 Лобовиков Сергей Дмитриевич/task3.c
+++ 3824Б1ПР6/16 Лобовиков Сергей Дмитриевич/task3.c
@@ -1,6 +1,15 @@
+#include <string.h>
 // Задача 3. Дана строка s, состоящая из английских слов, разделенных пробелами
 // (одним или несколькими). Определить количество слов в строке.
 int task3(char *s)
 {
-  return -1;
+  int size = strlen(s);
+  int answer;
+  if(s[0] == ' ') answer = 0;
+  else answer = 1;
+  for(int i = 1; i < size - 1; i++)
+  {
+    if(s[i] == ' ' && s[i+1] != ' ') answer++;
+  }
+  return answer;
 }
