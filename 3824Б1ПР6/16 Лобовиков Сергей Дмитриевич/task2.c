be4b29a10ccb60291b65f790c618df0fc44c556c
meowaura <118028413+meowaura@users.noreply.github.com>
2024-12-19 09:34:17 +0300

    Update task2.c

--- 3824Б1ПР6/16 Лобовиков Сергей Дмитриевич/task2.c
+++ 3824Б1ПР6/16 Лобовиков Сергей Дмитриевич/task2.c
@@ -4,5 +4,19 @@
 // Найти число серий в массиве.
 int task2(unsigned A[], size_t n)
 {
-  return -1;
+  int series_count = 0;
+  int temp = 0;
+  for(int i = 0; i < n - 1; i++)
+  {
+    if(A[i] == A[i+1])
+    {
+      temp++;
+    }else
+    {
+      if(temp >= 1) series_count++; 
+      temp = 0;
+    }
+  }
+  if(temp > 0) series_count++;
+  return series_count;
 }
